05e88745e0c33beadacde5a5a94f8c023b829081
Peter Pron <pron@peterson.fritz.box>
2015-02-16 22:47:34 +0100

    initial commit -- WIP/not working

---
+++ src/main/java/org/springframework/ldap/samples/useradmin/config/LdapConfiguration.java
@@ -0,0 +1,46 @@
+package org.springframework.ldap.samples.useradmin.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.repository.support.SimpleLdapRepository;
+import org.springframework.ldap.samples.useradmin.domain.User;
+import org.springframework.ldap.samples.useradmin.domain.UserRepo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pron
+ * Date: 19.01.15
+ * Time: 00:12
+ * To change this template use File | Settings | File Templates.
+ */
+
+@PropertySource("ldap.properties")
+@Configuration
+public class LdapConfiguration {
+
+//  Logger LOG = LoggerFactory.getLogger(LdapConfiguration.class);
+
+  @Autowired
+  private Environment env;
+
+  @Bean
+  public LdapContextSource contextSource() {
+    LdapContextSource contextSource = new LdapContextSource();
+    contextSource.setUrl(env.getRequiredProperty("sample.ldap.url"));
+    contextSource.setBase(env.getRequiredProperty("sample.ldap.base"));
+    contextSource.setUserDn(env.getRequiredProperty("sample.ldap.user"));
+    contextSource.setPassword(env.getRequiredProperty("sample.ldap.password"));
+    return contextSource;
+  }
+
+  @Bean
+  public LdapTemplate ldapTemplate() {
+    return new LdapTemplate(contextSource());
+  }
+
+}
